b4efeaecac71dcc513a1ba8984c15acea49dd87f
simplysoftware_de <67741468+simplysoftware-de@users.noreply.github.com>
2021-01-24 13:23:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ apt install software-properties-common -y
 add-apt-repository ppa:ondrej/php -y
 apt update -y
 apt install apache2 -y
-apt install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 php7.4-fpm -y
+apt install php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 php7.4-fpm php7.4-ssh2 -y
 apt install mariadb-server mariadb-client -y
 mysql_secure_installation
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
